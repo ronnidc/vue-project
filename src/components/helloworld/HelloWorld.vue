fa8d20ed8eb0f992dba19443ee9320e3e7b3d23b
Ronni Chidekel <ronni@kudo.dk>
2022-06-02 10:25:34 +0200

    first commit

---
+++ src/components/helloworld/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="greetings">
+        <h1 class="greetings-title green">{{ msg }}</h1>
+        <h3>
+            You’ve successfully created a project with
+            <a target="_blank" href="https://vitejs.dev/">Vite</a> +
+            <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
+        </h3>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    msg: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<style lang="scss">
+@import './helloworld.scss';
+</style>
